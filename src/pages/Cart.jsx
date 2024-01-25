11dd527b9df4e3bbf60a854ba0e9eccc7f05a917
JMGA <jmgaleman@gmail.com>
2024-01-25 18:22:28 -0300

    trabajando en el checkout falta lograr que se vean las imagenes y detalles de los products

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import ItemListContainer from "../components/ItemListContainer/ItemListContainer";
 
 export const Cart = () => {
     const {count} = useContext (CartContext)
 
     const idsList = count.map ((item) => item.id);
 
-    return count.length === 0 ? (<h1>no hay productos en el carrito</h1>) :null
+    return count.length === 0 ? (<h1>no hay productos en el carrito</h1>) : <ItemListContainer productsData = {count} />
 };
